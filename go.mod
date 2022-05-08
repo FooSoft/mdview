5f75ba5c3ea910a928d448d6d04f2e21bb9e99b2
Alex Yatskov <alex@foosoft.net>
2022-05-08 11:00:38 -0700

    Add styling

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/FooSoft/goldsmith v0.0.0-20220410034610-83530f1b2fe4
 	github.com/FooSoft/goldsmith-components v0.0.0-20220410175545-c35ca9cfd9d4
+	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/toqueteos/webbrowser v1.2.0
 	github.com/yuin/goldmark v1.4.12
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -8,12 +9,15 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
+	"strings"
 	"syscall"
 
 	"github.com/FooSoft/goldsmith"
 	"github.com/FooSoft/goldsmith-components/devserver"
+	"github.com/FooSoft/goldsmith-components/plugins/document"
 	"github.com/FooSoft/goldsmith-components/plugins/livejs"
 	"github.com/FooSoft/goldsmith-components/plugins/markdown"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/toqueteos/webbrowser"
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/extension"
@@ -21,9 +25,28 @@ import (
 	"github.com/yuin/goldmark/renderer/html"
 )
 
+//go:embed github-markdown.css
+var githubStyle string
+
+//go:embed github-fixup.css
+var githubFixup string
+
 type builder struct {
 	port     int
 	browsing bool
+}
+
+func embedCss(file *goldsmith.File, doc *goquery.Document) error {
+	var styleBuilder strings.Builder
+	styleBuilder.WriteString("<style type=\"text/css\">\n")
+	styleBuilder.WriteString(githubStyle)
+	styleBuilder.WriteString(githubFixup)
+	styleBuilder.WriteString("</style>")
+
+	doc.Find("body").AddClass("markdown-body")
+	doc.Find("head").SetHtml(styleBuilder.String())
+
+	return nil
 }
 
 func (self *builder) Build(contentDir, buildDir, cacheDir string) {
@@ -39,6 +62,7 @@ func (self *builder) Build(contentDir, buildDir, cacheDir string) {
 		Clean(true).
 		Chain(markdown.NewWithGoldmark(gm)).
 		Chain(livejs.New()).
+		Chain(document.New(embedCss)).
 		End(buildDir)
 
 	for _, err := range errs {
@@ -60,11 +84,6 @@ func main() {
 	}
 
 	requestPath := flag.Arg(0)
-	buildDir, err := ioutil.TempDir("", "mvd-*")
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	info, err := os.Stat(requestPath)
 	if err != nil {
 		log.Fatal(err)
@@ -75,6 +94,18 @@ func main() {
 		contentDir = filepath.Dir(requestPath)
 	}
 
+	buildDir, err := ioutil.TempDir("", "mvd-*")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		log.Println("cleaning up...")
+		if err := os.RemoveAll(buildDir); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
 	go func() {
 		b := &builder{port: *port}
 		devserver.DevServe(b, *port, contentDir, buildDir, "")
@@ -83,12 +114,5 @@ func main() {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	for {
-		<-sigs
-		log.Println("terminating...")
-		break
-	}
-	if err := os.RemoveAll(buildDir); err != nil {
-		log.Fatal(err)
-	}
+	<-sigs
 }
