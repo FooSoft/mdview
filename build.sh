fe86afed2eb4fdf8a2b37cd89e5c5227715b98ed
Alex Yatskov <alex@foosoft.net>
2022-05-08 18:01:00 -0700

    Fix build script

--- build.sh
+++ build.sh
@@ -6,17 +6,17 @@ export GOARCH=amd64
 export GOOS=windows
 go build -o ./build/$GOOS/mdview/ .
 pushd build/$GOOS
-7za a mdview.zip mdview
+7za a mdview.$GOOS.zip mdview
 popd
 
 export GOOS=linux
 go build -o ./build/$GOOS/mdview/ .
 pushd build/$GOOS
-tar czvf mdview.tar.gz mdview
+tar czvf mdview.$GOOS.tar.gz mdview
 popd
 
 export GOOS=darwin
 go build -o ./build/$GOOS/mdview/ .
 pushd build/$GOOS
-tar czvf mdview.tar.gz mdview
+tar czvf mdview.$GOOS.tar.gz mdview
 popd
